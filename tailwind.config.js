43b447c6799a6448da908f785660ec60831dd108
Jason Long <jason@jasonlong.me>
2021-01-05 21:34:17 -0500

    Add coming soon page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 2500ms linear',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': {
+            'opacity': '0',
+          },
+          '50%': {
+            'opacity': '0',
+          },
+          '100%': {
+            'opacity': '1',
+          }
+        },
+      },
+    },
   },
   variants: {
     extend: {},
